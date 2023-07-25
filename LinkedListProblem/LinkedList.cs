e75d095d4fe6d907313d6bc6b16a1e31d54a9a20
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-25 13:37:57 +0530

    [Riya] Add : Add in Reverse Order

--- LinkedListProblem/LinkedList.cs
+++ LinkedListProblem/LinkedList.cs
@@ -27,5 +27,33 @@ namespace LinkedListProblem
             }
             Console.WriteLine("{0} inserted into linked list", node.data);
         }
+        internal void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
