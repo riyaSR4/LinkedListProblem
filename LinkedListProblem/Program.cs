13b0c648f325eb9858520ade652fb113669a6f3b
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-25 12:11:17 +0530

    [Riya] Add : Folder Structure Created

---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List operations");
+        }
+    }
+}
