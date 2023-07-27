e4912122c737e388e05395b89d698bc1c58bf971
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-27 12:19:53 +0530

    [Riya] Add : Linked List Queue

---
+++ LinkedListProblem/LinkedListQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedListQueue
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty,Deletion is not possible");
+                return;
+            }
+            else
+            {
+                while (this.head != null)
+                {
+                    Console.WriteLine("Value Dequeued is {0}", this.head.data);
+                    this.head = this.head.next;
+                    break;
+                }
+            }
+        }
+    }
+}
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -31,16 +31,24 @@ namespace LinkedListProblem
             //Console.WriteLine("Delete Node");
             //list.DeleteNodeAtParticularPosition(2);
 
-            LinkedListStack stack = new LinkedListStack();
-            stack.Push(56);
-            stack.Push(30);
-            stack.Push(70);
+            //LinkedListStack stack = new LinkedListStack();
+            //stack.Push(56);
+            //stack.Push(30);
+            //stack.Push(70);
+            ////stack.Display();
+            //Console.WriteLine("Peek");
+            //stack.Peek();
+            //Console.WriteLine("Pop");
+            //stack.Pop();
             //stack.Display();
-            Console.WriteLine("Peek");
-            stack.Peek();
-            Console.WriteLine("Pop");
-            stack.Pop();
-            stack.Display();
+
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+            queue.Dequeue();
+            queue.Display();
         }
     }
 }
